02dae47266baa2e4cf51ae08fcaec0b96bbed4a1
Elsa Fransiskus <felia.sionchristian@gmail.com>
2025-12-23 14:34:28 +0700

    fix price fetch and storing

--- src/models/watchlist.model.js
+++ src/models/watchlist.model.js
@@ -1,5 +1,23 @@
 const { supabase } = require("../config/supabase");
 
+function cleanSymbol(symbol) {
+  const [symbolOnly, exchange] = symbol.split(".");
+  return { symbolOnly, exchange };
+}
+
+exports.checkExists = async (symbol) => {
+  const { symbolOnly } = cleanSymbol(symbol);
+
+  const { data, error } = await supabase
+    .from("watchlist")
+    .select("*")
+    .eq("symbol", symbolOnly)
+    .limit(1);
+
+  if (error) throw new Error(error.message);
+  return data?.length > 0;
+};
+
 exports.add = async (symbol) => {
   const [symbolOnly, exchange] = symbol.split(".");
   console.log(symbolOnly, exchange);
